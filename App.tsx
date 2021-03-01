26270f5750e9268ec93def26b11b98fe25bcd18f
iMac <chandima.hosting@gmail.com>
2021-03-01 12:51:52 +0530

    add

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Platform, Dimensions, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Platform, Dimensions, SafeAreaView, TouchableOpacity, Text, Alert } from 'react-native';
 import MapView, { Marker, AnimatedRegion, MarkerAnimated, PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
 
 const { width, height } = Dimensions.get('window');
 
@@ -10,6 +12,23 @@ const LATITUDE = 37.78825;
 const LONGITUDE = -122.4324;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQp40FcHWzUoojgd9ZPpkIjj5nMwSwjd8",
+  authDomain: "livelocation-1a3b6.firebaseapp.com",
+  databaseURL: "https://livelocation-1a3b6-default-rtdb.firebaseio.com",
+  projectId: "livelocation-1a3b6",
+  storageBucket: "livelocation-1a3b6.appspot.com",
+  messagingSenderId: "430825290866",
+  appId: "1:430825290866:web:5abb21d38d585a982016eb",
+  measurementId: "G-GJN1X70ZF9"
+};
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+const driverCollection = firestore().collection('Driver');
 
 export default class Trackee extends React.Component {
 
@@ -39,12 +58,34 @@ export default class Trackee extends React.Component {
     // this.pubnub.init(this);
   }
 
+  addDriver = (latitude:number,longitude:number) => {
+    driverCollection.add({latlng:{latitude,longitude}});
+    Alert.alert('--- Driver added ---');
+  }
+
+  setDriver = (driverid:string, latitude: number, longitude: number) => {
+    driverCollection.doc(driverid).set({latnlg:{latitude,longitude}});
+    Alert.alert('--- Driver set success ---');
+  }
+
+  updateDriver = (driverid:string, latitude: number, longitude: number) => {
+    driverCollection.doc(driverid).update({latnlg:{latitude,longitude}});
+    Alert.alert('--- Driver set success ---');
+  }
+
+  deleteDriver = (driverid:string) => {
+    driverCollection.doc(driverid).delete().then(()=>{Alert.alert('--- Driver set success ---')}).catch((err)=>{Alert.alert(err)});
+  }
+
+
+
   componentDidMount() {
     this.watchLocation();
   }
 
   componentDidUpdate(prevProps: any, prevState: any) {
     if (this.state.latitude !== prevState.latitude) {
+      this.addDriver(this.state.latitude,this.state.longitude)
       // this.pubnub.publish({
       //   message: {
       //     latitude: this.state.latitude,
@@ -89,7 +130,7 @@ export default class Trackee extends React.Component {
         enableHighAccuracy: true,
         timeout: 20000,
         maximumAge: 1000,
-        distanceFilter: 30,
+        distanceFilter: 2,
       }
     );
   };
@@ -113,6 +154,9 @@ export default class Trackee extends React.Component {
               coordinate={this.state.coordinate}
             />
           </MapView>
+          {/* <TouchableOpacity style={{position:'absolute', top:height*7/10, backgroundColor:'#3e8f', height:height/15, width:width/2, marginBottom:20, padding:10}} onPress={()=>this.setDriver('pasindu1003',this.state.latitude,this.state.longitude)} ><Text>Set</Text></TouchableOpacity>
+          <TouchableOpacity style={{position:'absolute', top:height*8/10, backgroundColor:'#3e8f', height:height/15, width:width/2, marginBottom:20, padding:10}} onPress={()=>this.updateDriver('pasindu1003',this.state.latitude,this.state.longitude)} ><Text>Update</Text></TouchableOpacity>
+          <TouchableOpacity style={{position:'absolute', top:height*9/10, backgroundColor:'#3e8f', height:height/15, width:width/2, marginBottom:20, padding:10}} onPress={()=>this.deleteDriver('pasindu1003')} ><Text>Delete</Text></TouchableOpacity> */}
         </View>
       </SafeAreaView>
     );
